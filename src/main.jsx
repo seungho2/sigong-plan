cba85f4f687da8b926a2c5b1f6de9efffcbf7885
Seungho Yang <seungho2@gmail.com>
2026-01-13 00:12:47 -0500

    title에 test 서버 표시

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
 
+if (import.meta.env.DEV) {
+  document.title = "SI-GONG PLAN (localhost)";
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ErrorBoundary>
